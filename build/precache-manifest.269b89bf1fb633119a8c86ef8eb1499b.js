30af707babc3ba082b33080f95d49ed5d126806d
khangbeo <khangbeo2012@gmail.com>
2021-09-20 00:31:42 -0700

    Added README

---
+++ build/precache-manifest.269b89bf1fb633119a8c86ef8eb1499b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82844edf60fc0576ca5e1a7fbdfeca50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2f3aefbc088c918c60",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2f3aefbc088c918c60",
+    "url": "/static/js/main.e89ef791.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
