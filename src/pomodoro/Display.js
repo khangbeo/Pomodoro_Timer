2330ee17ddf0153cf3cf376ef5833e6a2c901131
Khang <khangbeo2012@gmail.com>
2025-03-23 05:40:30 -0700

    updated node version

--- src/pomodoro/Display.js
+++ src/pomodoro/Display.js
@@ -18,33 +18,6 @@ function nextTick(prevState) {
     };
 }
 
-/**
- * Higher order function that returns a function to update the session state with the next session type upon timeout.
- * @param focusDuration
- *    the current focus duration
- * @param breakDuration
- *    the current break duration
- * @returns
- *  function to update the session state.
- */
-function nextSession(focusDuration, breakDuration) {
-    /**
-     * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
-     */
-    return (currentSession) => {
-        if (currentSession.label === "Focusing") {
-            return {
-                label: "On Break",
-                timeRemaining: breakDuration * 60,
-            };
-        }
-        return {
-            label: "Focusing",
-            timeRemaining: focusDuration * 60,
-        };
-    };
-}
-
 export default function Display({
     isTimerRunning,
     activeState,
